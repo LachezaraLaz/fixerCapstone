de00f415c426cd99e18291c8856da05b84cf68b5
Facu-alfaro <fn_alfaro@hotmail.com>
2024-10-11 17:17:38 -0400

    Javascript organization to provide a shell for frontend development. Allows Navigation between different pages.

--- adminClient/App.js
+++ adminClient/App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Import your pages (screens)
+import HomeScreen from './src/homeScreen';
+import DetailsScreen from './src/detailsScreen';
+
+// Create a Stack Navigator
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hey this is Fixer Admin!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

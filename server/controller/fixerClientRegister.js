11bd0661486a9b7830694bb9ef43b0029de81b26
mkandaleft <markandaleft@hotmail.com>
2024-10-13 15:18:30 -0400

    Added server side client registration to create a document in MongoDB

---
+++ server/controller/fixerClientRegister.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcrypt');
+const fixerClientObject = require('../model/fixerClientModel');
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const registerUser = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    // Check if user already exists
+    const existedUser = await fixerClientObject.fixerClient.findOne({ email });
+    if (existedUser) {
+        return res.status(400).send({ statusText: 'user already exists' });
+    }
+
+    // Hash password
+    const encryptedPassword = await bcrypt.hash(password, 12);
+
+    // Try to create new user
+    try {
+        await fixerClientObject.fixerClient.create({
+            username,
+            email,
+            password: encryptedPassword,
+            approved: false,
+            accountType: 'client'
+        });
+        res.send({ status: 'success', data: 'user created successfully' });
+    } catch (e) {
+        res.send({ status: 'error', data: 'user creation failed' });
+    }
+};
+
+module.exports = { registerUser };
---
+++ server/model/fixerClientModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const fixerClientModel = new mongoose.Schema({
+    username: String,
+    email: {type: String, unique: true, required: true},
+    password: String,
+    approved: Boolean,
+    approvedAt: Date,
+    accountType: String
+},{
+    collection: 'fixerClientInfo'
+});
+
+const fixerClient = mongoose.model('fixerClientInfo', fixerClientModel);
+
+module.exports = {fixerClient};

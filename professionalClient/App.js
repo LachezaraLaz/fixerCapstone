3783156b7d42ea8fe8ff4d8e9c38f9b6df085288
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2024-10-13 11:33:35 -0400

    App.js fixed because of a conflict in the pull request

--- professionalClient/App.js
+++ professionalClient/App.js
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// Import your pages (screens)
-// import HomeScreen from './src/homeScreen';
-// import DetailsScreen from './src/detailsScreen';
-// import ProfilePage from "./src/profilePage";
 import welcomePage from "./src/welcomePage";
 import SignInPage from "./src/signinPage";
 import SignUpPage from "./src/signupPage";
@@ -17,9 +12,6 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="welcomePage">
-                {/*<Stack.Screen name="Home" component={HomeScreen} />*/}
-                {/*<Stack.Screen name="Detail" component={DetailsScreen} />*/}
-                {/*<Stack.Screen name="ProfilePage" component={ProfilePage} /> //DLU part*/}
                 <Stack.Screen name="welcomePage" component={welcomePage} />
                 <Stack.Screen name="SignInPage" component={SignInPage} />
                 <Stack.Screen name="SignUpPage" component={SignUpPage} />

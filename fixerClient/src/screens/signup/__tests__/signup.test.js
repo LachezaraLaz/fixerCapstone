6d6b27062b2425e5cb83e2e61807f80db2d49b27
mkandaleft <markandaleft@hotmail.com>
2024-10-15 22:38:15 -0400

    Added the same test cases for fixerClient

--- fixerClient/src/screens/signup/__tests__/signup.test.js
+++ fixerClient/src/screens/signup/__tests__/signup.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react-native';
-import SignUpPage from '../SignUpPage'; // Adjust the import path as necessary
+import SignUpPage from '../signupPage'; // Adjust the import path as necessary
 import { Alert } from 'react-native';
+import axios from 'axios';
 
 // Mock the Alert.alert function
 jest.spyOn(Alert, 'alert');
+jest.mock('axios');
+
+beforeEach(() => {
+    // Clear all previous mocks
+    jest.clearAllMocks();
+
+    // Set up fresh mocks
+    axios.post.mockResolvedValue({ status: 200 });
+});
 
 test('shows "All fields are required" alert when fields are empty', async () => {
     const { getByTestId } = render(<SignUpPage />);
@@ -16,5 +26,40 @@ test('shows "All fields are required" alert when fields are empty', async () => 
     // Wait for the alert to be called
     await waitFor(() => {
         expect(Alert.alert).toHaveBeenCalledWith('Error', 'All fields are required');
+    });
+});
+
+test('shows "Passwords do not match" alert when passwords do not match', async () => {
+    const { getByPlaceholderText, getByTestId } = render(<SignUpPage />);
+
+    fireEvent.changeText(getByPlaceholderText('Email'), 'test@example.com');
+    fireEvent.changeText(getByPlaceholderText('Password'), '123456');
+    fireEvent.changeText(getByPlaceholderText('Confirm Password'), '654321');
+    const signUpButton = getByTestId('sign-up-button');
+    fireEvent.press(signUpButton);
+
+    await waitFor(() => {
+        expect(Alert.alert).toHaveBeenCalledWith('Error', 'Passwords do not match');
+    });
+});
+
+test('successfully creates an account', async () => {
+    const mockNavigation = { navigate: jest.fn() };
+    const { getByPlaceholderText, getByTestId } = render(<SignUpPage navigation={mockNavigation} />);
+
+    // Simulate user input
+    fireEvent.changeText(getByPlaceholderText('Email'), 'user@example.com');
+    fireEvent.changeText(getByPlaceholderText('First Name'), 'John');
+    fireEvent.changeText(getByPlaceholderText('Last Name'), 'Doe');
+    fireEvent.changeText(getByPlaceholderText('Password'), 'password123');
+    fireEvent.changeText(getByPlaceholderText('Confirm Password'), 'password123');
+
+    // Simulate button press
+    const signUpButton = getByTestId('sign-up-button');
+    fireEvent.press(signUpButton);
+
+    // Assertions
+    await waitFor(() => {
+        expect(Alert.alert).toHaveBeenCalledWith("Account created successfully");
     });
 });

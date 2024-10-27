5911ad53f9bceb039ef13c8b749534ebecc26814
thaneekan <92413776+thaneekan@users.noreply.github.com>
2024-10-27 14:30:56 -0400

    set-up backend for client profilePage.js but it is giving network error message. will be fixing this soon.

--- server/routes/fixerClientRoute.js
+++ server/routes/fixerClientRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { registerUser } = require('../controller/fixerClientRegister');
-const {signinUser} = require("../controller/fixerClientSignIn");
+const { signinUser } = require("../controller/fixerClientSignIn");
+const { profile, authenticateJWT } = require('../controller/fixerClientProfile');
 const fixerClientRouter = express.Router();
 
 // Register user route
 fixerClientRouter.post('/register', registerUser);
 fixerClientRouter.post('/signin', signinUser);
+fixerClientRouter.get('/profile', authenticateJWT, profile)
 
 module.exports = { fixerClientRouter };

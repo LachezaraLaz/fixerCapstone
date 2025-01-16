3c6b5feb5dcec1aa3b6d27aa33fa82ef80865af4
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2025-01-16 13:44:04 -0500

    set the level of logs functionality added

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,17 +1,28 @@
 const pino = require('pino');
 const transport = pino.transport({
     target: '@logtail/pino',
-    options: {sourceToken: process.env.BETTER_STACK_TOKEN }
-})
-const logger = pino({
-    customLevels: {emergency:0}, // emergency is activated when we have an issue to fix
-    level: process.env.PINO_LOG_LEVEL || 'info',
-    redact:{paths: ['email', 'password', 'token', process.env.JWT_SECRET],remove: true}, // disallow these to be logged
-    formatters: {
-        level: (label) => {
-            return{level: label.toUpperCase()};
-        }
-    }
-},transport);
+    options: { sourceToken: process.env.BETTER_STACK_TOKEN }
+});
 
-module.exports = {logger};
+let currentLogLevel = process.env.PINO_LOG_LEVEL || 'info';
+
+const logger = pino(
+    {
+        customLevels: { server: 80, emergency: 100 },
+        level: currentLogLevel,
+        redact: { paths: ['email', 'password', 'token', process.env.JWT_SECRET], remove: true },
+        formatters: {
+            level: (label) => ({ level: label.toUpperCase() })
+        }
+    },
+    transport
+);
+
+// Function to dynamically set log level
+const setLogLevel = (level) => {
+    logger.level = level;
+    currentLogLevel = level;
+    logger.info(`Log level changed to ${level}`);
+};
+
+module.exports = { logger, setLogLevel };

e84f581299cf17b3ff693081305ff4e8923b4888
mkandaleft <markandaleft@hotmail.com>
2025-01-16 18:16:22 -0500

    Replaced hard coded user data with dynamic fetching of data from the backend. Also re-structured the stack in App.js to avoid Stream connectUser() before sign in.

--- fixerClient/src/screens/chat/chatConfig.js
+++ fixerClient/src/screens/chat/chatConfig.js
@@ -1,4 +1,1 @@
-export const chatApiKey = "ut27s54hv26w";
-export const chatUserId = "676494c50b8c84877109e240";
-export const chatUserName = "NicolaTest";
-export const chatUserToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjc2NDk0YzUwYjhjODQ4NzcxMDllMjQwIn0.WQWSlGFNdOJ-5LudMiJyZkXjI0J0tGgUI9L7Y5HZ3ck";
+export const STREAM_API_KEY = "ut27s54hv26w";
--- fixerClient/src/screens/chat/chatListPage.js
+++ fixerClient/src/screens/chat/chatListPage.js
@@ -1,20 +1,18 @@
 import React from "react";
 import { ChannelList } from "stream-chat-expo";
-import { chatApiKey, chatUserId } from './chatConfig';
 import { useChatContext } from './chatContext';
 
 const ChannelListScreen = ({ navigation }) => {
 
-    const { setChannel } = useChatContext();
+    const { setChannel, user } = useChatContext();
 
     const handleSelectChannel = (channel) => {
         setChannel(channel);
         navigation.navigate("ChatPage");
     };
-
     const filters = {
         members: {
-            '$in': [chatUserId]
+            '$in': [ user?.id || '' ]
         },
     };
 

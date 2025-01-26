331bb65d3a97bce3145b3ab57533d668060850c6
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2025-01-26 11:13:07 -0500

    Adding an import for the setting page that is causing an error

--- fixerClient/src/NavBarComponent.js
+++ fixerClient/src/NavBarComponent.js
@@ -6,6 +6,7 @@ import CreateIssue from './screens/createIssue/createIssue';
 import MyIssuesPosted from "./screens/myIssuesPosted/myIssuesPosted";
 import ChatListPage from '../src/screens/chat/chatListPage';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import SettingsPage from '../src/screens/settingsPage/settingsPage';
 
 const Tab = createBottomTabNavigator();
 

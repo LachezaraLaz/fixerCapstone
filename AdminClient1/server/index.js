133782c807b94806898f2eb1d2e43a57174dcd11
LachezaraLaz <122994431+LachezaraLaz@users.noreply.github.com>
2025-01-29 18:21:00 -0500

    removed commented out code from index.js admin

--- AdminClient1/server/index.js
+++ AdminClient1/server/index.js
@@ -1,23 +1,3 @@
-// const mongoose = require('mongoose');
-// require('dotenv').config(); 
-
-// const connectDB = async () => {
-//     console.log("Server is starting...");
-
-//     try {
-//         await mongoose.connect(process.env.MONGO_URL, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         });
-//         console.log('MongoDB Connected Successfully');
-//     } catch (error) {
-//         console.error('MongoDB Connection Error:', error);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
-
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');

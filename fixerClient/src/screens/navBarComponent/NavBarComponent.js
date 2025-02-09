6621349b23f91d00d143127658f330f4af6c8fc5
El-Mirrio <116108802+El-Mirrio@users.noreply.github.com>
2025-02-08 19:18:03 -0500

    - Change design Color from Blue to Orange

--- fixerClient/src/screens/navBarComponent/NavBarComponent.js
+++ fixerClient/src/screens/navBarComponent/NavBarComponent.js
@@ -12,30 +12,28 @@ const Tab = createBottomTabNavigator();
 
 export default function NavBar({ setIsLoggedIn }) {  // Receive setIsLoggedIn as a prop
     return (
-        <Tab.Navigator screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
 
-                if (route.name === 'Home') {
-                    iconName = focused ? 'home' : 'home-outline';
-                } else if (route.name === 'JobsPosted') {
-                    iconName = focused ? 'hammer' : 'hammer-outline';
-                }
-                else if (route.name === 'CreateIssue') {
-                    iconName = focused ? 'add' : 'add-outline';
-                }
-                else if (route.name === 'Chat') {
-                    iconName = focused ? 'chatbubble' : 'chatbubble-outline';
-                }
-                else if (route.name === 'Settings') {
-                    iconName = focused ? 'settings' : 'settings-outline';
-                }
+                    if (route.name === 'Home') {
+                        iconName = focused ? 'home' : 'home-outline';
+                    } else if (route.name === 'JobsPosted') {
+                        iconName = focused ? 'hammer' : 'hammer-outline';
+                    } else if (route.name === 'CreateIssue') {
+                        iconName = focused ? 'add' : 'add-outline';
+                    } else if (route.name === 'Chat') {
+                        iconName = focused ? 'chatbubble' : 'chatbubble-outline';
+                    } else if (route.name === 'Settings') {
+                        iconName = focused ? 'settings' : 'settings-outline';
+                    }
 
-                return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: 'blue',
-            tabBarInactiveTintColor: 'gray',
-        })}
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: 'orange',
+                tabBarInactiveTintColor: 'gray',
+            })}
         >
             {/* Pass setIsLoggedIn to HomeScreen */}
             <Tab.Screen name="Home">

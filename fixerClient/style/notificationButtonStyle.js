39f0670e25061e4772288d478308e6b0c145660d
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2025-02-10 19:27:19 -0500

    new search bar and notification button as reusable components

---
+++ fixerClient/style/notificationButtonStyle.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+notificationButton: {
+    width: 40,
+    height: 40,
+    backgroundColor: '#FAF9F6',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 0
+}
+});
+
+export default styles;

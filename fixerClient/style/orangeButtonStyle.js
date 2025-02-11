d7856ba90533d8886f32acbd17db9b636f8d2019
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2025-02-10 19:26:17 -0500

    creating a global button that could be used everywhere in the code

---
+++ fixerClient/style/orangeButtonStyle.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 14,
+        borderRadius: 8,
+        flex: 1,
+        justifyContent: 'center',
+        minWidth: '90%',
+        alignSelf: 'center',
+        alignItems: 'center',
+        shadowRadius: 4,
+        elevation: 5
+    },
+    text: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
+
+export default styles;

e9b81748c2a93e7bf807d15b83e5d7031bf1b55f
ishaicohen <ishai.cohen18@gmail.com>
2025-02-26 16:15:22 -0500

    added entry points for the VerificationStatus.jsx and VerifyEmailPage.jsx, issue does not seem to have anything to do with App.jsx

--- AdminClient1/src/App.jsx
+++ AdminClient1/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import SignUpPage from "./screens/SignUpPage";
 import SignInPage from "./screens/SignInPage";
+import VerificationStatus from "./screens/VerificationStatus";
+import VerifyEmailPage from "./screens/VerifyEmailPage";
 
 function App() {
     return (
@@ -9,6 +11,8 @@ function App() {
             <Routes>
                 <Route path="/signup" element={<SignUpPage />} />
                 <Route path="/signin" element={<SignInPage />} />
+                <Route path="/verification-status" element={<VerificationStatus />} />
+                <Route path="/verify-email" element={<VerifyEmailPage />} />
                 <Route path="/" element={<Navigate to="/signup" replace />} />
             </Routes>
         </Router>

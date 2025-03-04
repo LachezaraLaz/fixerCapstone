a56cf1e8148fd1a2a803e1e565dee8395c98832d
dlu2002 <al_delu@live.concordia.ca>
2025-03-04 18:05:40 -0500

    Update squareConfig.js
    Fixed issues where professional's square accounts were not correctly merging with their mongodb entries.

--- server/utils/squareConfig.js
+++ server/utils/squareConfig.js
@@ -1,16 +1,3 @@
-// const { Client } = require('square');
-// const dotenv = require('dotenv');
-//
-// // Load environment variables
-// dotenv.config();
-//
-// const client = new Client({
-//     accessToken: process.env.SQUARE_ACCESS_TOKEN,
-//     environment: process.env.SQUARE_ENVIRONMENT === 'production' ? 'production' : 'sandbox',
-// });
-//
-// module.exports = client;
-
 require('dotenv').config();
 const { SquareClient, SquareEnvironment } = require('square');
 

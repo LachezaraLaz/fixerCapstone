0ba428b9474713ba1ed5b4e544b880fd0952513b
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2025-03-05 22:50:26 -0500

    Refactor the getAllIssues.js to add the dto and repository #220

--- server/controller/getAllIssues.js
+++ server/controller/getAllIssues.js
@@ -1,10 +1,13 @@
-const { Jobs } = require('../model/createIssueModel');
+const { getAllJobs } = require('../../server/controller/issueRepository');
+const { issueDTO } = require('../../server/controller/issueDTO');
 
 // GET /issues route to fetch all jobs
 const getAllIssues = async (req, res) => {
     try {
-        const jobs = await Jobs.find();
-        res.status(200).json({ jobs });
+        const jobs = await getAllJobs();
+        const formattedJobs = jobs.map(job => issueDTO(job));
+
+        res.status(200).json({ jobs: formattedJobs });
     } catch (error) {
         res.status(500).json({ message: 'Failed to fetch jobs', error: error.message });
     }

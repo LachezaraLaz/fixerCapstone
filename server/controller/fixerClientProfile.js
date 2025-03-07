8b57c7193e2a4850d4c056bb96de51a24681a625
mkandaleft <markandaleft@hotmail.com>
2025-03-06 21:41:50 -0500

    Added initial docs to test if it works on signin client/professional and server

--- server/controller/fixerClientProfile.js
+++ server/controller/fixerClientProfile.js
@@ -1,7 +1,23 @@
 const jwt = require('jsonwebtoken');  // Make sure this is required to use JWT verification
 const fixerClientObject = require('../model/fixerClientModel'); // Mongoose model for professional
 
-// Middleware to authenticate JWT
+/**
+ * @module server/controller/fixerClientProfile
+ */
+
+/**
+ * Middleware to authenticate JWT token from the request headers.
+ * 
+ * @param {Object} req - Express request object.
+ * @param {Object} req.headers - Request headers.
+ * @param {string} req.headers.authorization - Authorization header containing the JWT token.
+ * @param {Object} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ * 
+ * @returns {Object} - Returns a 401 status with a message 'Unauthorized' if no token is provided or if the token is invalid.
+ *                     Returns a 403 status with a message 'Forbidden' if the token verification fails.
+ *                     Proceeds to the next middleware or route handler if the token is valid.
+ */
 const authenticateJWT = (req, res, next) => {
     const authorizationHeader = req.headers.authorization;  // Get authorization header
 
@@ -24,7 +40,17 @@ const authenticateJWT = (req, res, next) => {
     });
 };
 
-// Profile fetching function
+/**
+ * Fetches the profile of a client based on the user ID from the JWT token.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} req.user - The user object containing the user ID.
+ * @param {string} req.user.id - The ID of the user.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves to void.
+ *
+ * @throws {Error} - If there is an error fetching the client data.
+ */
 const profile = async (req, res) => {
     try {
         // Find the professional by their user ID from the JWT token

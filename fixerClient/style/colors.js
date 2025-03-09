6ade665d68c77f7b414c96726b6b518b15f64737
LachezaraLaz <lachezara.laz@gmail.com>
2025-03-09 18:11:38 -0400

    added new colors for (green and red)

--- fixerClient/style/colors.js
+++ fixerClient/style/colors.js
@@ -14,6 +14,36 @@ const colors = {
 
         darker: '#552f00',
     },
+    green: {
+        light: '#e6f4ea',
+        lightHover: '#d9efe0',
+        lightActive: '#b0dfc2',
+
+        normal: '#28a745',
+        normalHover: '#23963d',
+        normalActive: '#1e8535',
+
+        dark: '#1e7e34',
+        darkHover: '#186529',
+        darkActive: '#124c1e',
+
+        darker: '#0f3a17',
+    },
+    red: {
+        light: '#f8e6e6',
+        lightHover: '#f5d9d9',
+        lightActive: '#eeb0b0',
+
+        normal: '#dc3545',
+        normalHover: '#c6303e',
+        normalActive: '#b02a37',
+
+        dark: '#a71d2a',
+        darkHover: '#861722',
+        darkActive: '#65111a',
+
+        darker: '#4d0d14',
+    },
 };
 
 export default colors;

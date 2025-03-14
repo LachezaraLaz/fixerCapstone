b9556a9d2d02738b4254120eb90b3f40f61fca6c
dlu2002 <al_delu@live.concordia.ca>
2025-03-14 13:35:59 -0400

    Removed square files
    Problem encountered with Square:
    
    - No API to access credit cards with Square
    - Professionals must create Square accounts for payments to work between us and them
    
    Due to these problems, we have made the decision to switch from Square to Stripe. These changes reflect the removal of Square from the system.

--- server/routes/paymentRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { linkProfessionalAccount } = require('../controller/paymentController');
-const router = express.Router();
-
-// Link a professional’s Square account
-router.post('/link', linkProfessionalAccount);
-
-module.exports = router;

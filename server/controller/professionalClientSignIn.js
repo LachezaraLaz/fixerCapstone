e2660bfb77eee04121a32a86e338091bb484c912
SamyArab <samy013@outlook.com>
2025-03-17 07:23:14 -0400

    Integrating custom error handling in professionalClientSignIn.js #244

--- server/controller/professionalClientSignIn.js
+++ server/controller/professionalClientSignIn.js
@@ -1,5 +1,6 @@
 const ProfessionalDTO = require('../DTO/professionalDTO');
 const professionalRepository = require('../repository/professionalRepository');
+const AppError = require('../utils/AppError');
 
 /**
  * @module server/controller
@@ -16,42 +17,46 @@ const professionalRepository = require('../repository/professionalRepository');
  * @returns {Promise<void>} - Sends a response with the JWT token, stream token, user ID, and user name if successful.
  */
 const signinUser = async (req, res) => {
-    const { email, password } = req.body;
-    const professionalData = new ProfessionalDTO({ email, password });
+    try {
+        const { email, password } = req.body;
+        const professionalData = new ProfessionalDTO({ email, password });
 
-    // Check if user exists and is a professional
-    const user = await professionalRepository.findProfessionalByEmail(professionalData.email);
-    if (!user || user.accountType !== 'professional') {
-        return res.status(400).send({ statusText: 'User not found' });
+        // Check if user exists and is a professional
+        const user = await professionalRepository.findProfessionalByEmail(professionalData.email);
+        if (!user || user.accountType !== 'professional') {
+            throw new AppError('User not found', 400);
+        }
+
+        if (!user.verified) {
+            throw new AppError('Account not verified yet', 403);
+        }
+
+        // Compare password
+        const validPassword = await professionalRepository.comparePassword(professionalData.password, user.password);
+        if (!validPassword) {
+            throw new AppError('Invalid password', 400);
+        }
+
+        // Create JWT token
+        const token = professionalRepository.generateToken({
+            id: user._id,
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName
+        });
+
+        await professionalRepository.upsertStreamUser(user);
+
+        const streamToken = professionalRepository.createStreamToken(user._id);
+        res.send({
+            token,
+            streamToken,
+            userId: user._id.toString(),
+            userName: `${user.firstName} ${user.lastName}`
+        });
+    } catch (error) {
+        next(error);
     }
-
-    if (!user.verified) {
-        return res.status(403).send({ statusText: 'Account not verified yet' });
-    }
-
-    // Compare password
-    const validPassword = await professionalRepository.comparePassword(professionalData.password, user.password);
-    if (!validPassword) {
-        return res.status(400).send({ statusText: 'Invalid password' });
-    }
-
-    // Create JWT token
-    const token = professionalRepository.generateToken({
-        id: user._id,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName
-    });
-
-    await professionalRepository.upsertStreamUser(user);
-
-    const streamToken = professionalRepository.createStreamToken(user._id);
-    res.send({
-        token,
-        streamToken,
-        userId: user._id.toString(),
-        userName: `${user.firstName} ${user.lastName}`
-    });
 };
 
 module.exports = { signinUser };

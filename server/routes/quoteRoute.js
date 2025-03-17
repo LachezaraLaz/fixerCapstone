98eda0f1a24cbbfbfb605b89685909c8c83d1cc9
dlu2002 <al_delu@live.concordia.ca>
2025-03-17 13:29:26 -0400

    Update quoteRoute.js
    Added route to check the professionals' bank accounts (if they are set up)

--- server/routes/quoteRoute.js
+++ server/routes/quoteRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { submitQuote, authenticateJWT, getQuotesByJob, updateQuoteStatus } = require('../controller/submitQuote');
+const checkBankAccount = require('../controller/checkBankAccount');
 const quoteRouter = express.Router();
 
-quoteRouter.post('/create', authenticateJWT, submitQuote);
+quoteRouter.post('/create', authenticateJWT, checkBankAccount, submitQuote);
 quoteRouter.get('/job/:jobId', authenticateJWT, getQuotesByJob);
 quoteRouter.put('/:quoteId', authenticateJWT, updateQuoteStatus);
 

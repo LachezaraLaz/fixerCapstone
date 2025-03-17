c4a6a6f60cdec26b94650bcf96c785398671714b
ishaicohen <ishai.cohen18@gmail.com>
2025-03-17 14:49:07 -0400

    enabled CORS to support front end and backend running on different origins

--- AdminClient1/server/index.js
+++ AdminClient1/server/index.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const adminRoute = require("./routes/fixerAdminRoutes");
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use(cors());
 
 const PORT = process.env.PORT || 5003; // Change the port if needed
 

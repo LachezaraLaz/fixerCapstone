2d6623102e96feefc8c5ce82092e09f5f11d2a15
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2025-03-18 11:21:07 -0400

    configurations for translation + test on the welcome page

---
+++ fixerClient/localization.js
@@ -0,0 +1,13 @@
+const en = {
+    sign_in: "Sign In",
+    sign_up: "Sign Up",
+    welcome: "Welcome to",
+}
+
+const fr = {
+    sign_in: "Se connecter",
+    sign_up: "S'inscrire",
+    welcome: "Bienvenue à",
+}
+
+export {en, fr}

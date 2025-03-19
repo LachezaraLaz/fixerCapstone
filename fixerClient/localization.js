bb9ac1420e4c49fb3df370f95abf317d613a5f79
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2025-03-18 21:42:31 -0400

    translation for signup and removing unecessary imports from signin #247

--- fixerClient/localization.js
+++ fixerClient/localization.js
@@ -1,6 +1,8 @@
 const en = {
     sign_in: "Sign In",
     sign_up: "Sign Up",
+    first_name: "First Name",
+    last_name: "Last Name",
     email: "Email",
     password: "Password",
     welcome: "Welcome to",
@@ -8,11 +10,21 @@ const en = {
     forgot_password: "Forgot Password?",
     do_not_have_an_account: "Don't have an account? Sign up",
     back: "Back",
+    valid_address: "Please enter a valid email address",
+    confirm_password: "Confirm Password",
+    next: "Next",
+    street_address: "Street Address",
+    postal_code: "Postal Code",
+    verify_address: "Verify Address",
+    already_have_an_account:"Already have an account? Sign in",
+
 }
 
 const fr = {
     sign_in: "Se connecter",
     sign_up: "S'inscrire",
+    first_name: "Prénom",
+    last_name: "Nom de Famille",
     email: "Courriel",
     password: "Mot de Passe",
     welcome: "Bienvenue à",
@@ -20,6 +32,13 @@ const fr = {
     forgot_password: "Oubli du mot de passe?",
     do_not_have_an_account: "Si vous n'avez pas encore de compte, créez-en un",
     back: "Retourner",
+    valid_address:"Veuillez indiquer une adresse de courriel valide",
+    confirm_password: "Confirmer le mot de passe",
+    next: "Suivant",
+    street_address: "Adresse Civique",
+    postal_code: "Code Postal",
+    verify_address: "Vérifier l'adresse",
+    already_have_an_account:"Vous êtes déjà inscrit? Se connecter"
 }
 
 export {en, fr}

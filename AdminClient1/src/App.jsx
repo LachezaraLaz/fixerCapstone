048cd3a74a47b51b17e4d078b33f28fc5b44fc8a
ishaicohen <ishai.cohen18@gmail.com>
2025-03-18 23:16:33 -0400

    added admin dashboard element

--- AdminClient1/src/App.jsx
+++ AdminClient1/src/App.jsx
@@ -4,6 +4,7 @@ import SignUpPage from "./screens/SignUpPage";
 import SignInPage from "./screens/SignInPage";
 import VerificationStatus from "./screens/VerificationStatus";
 import VerifyEmailPage from "./screens/VerifyEmailPage";
+import AdminDashboard from "./screens/AdminDashboard";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/signin" element={<SignInPage />} />
                 <Route path="/verification-status" element={<VerificationStatus />} />
                 <Route path="/verify-email" element={<VerifyEmailPage />} />
+                <Route path="/admin-dashboard" element={<AdminDashboard />} />
                 <Route path="/" element={<Navigate to="/signup" replace />} />
             </Routes>
         </Router>
